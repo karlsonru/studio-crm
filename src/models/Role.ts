88473020c0ce8e148e40752fb2833808dc7b2f66
karlsonru <ivpolovnikov@hotmail.com>
2022-10-30 12:12:56 +0300

    feat: add react

--- src/models/Role.ts
+++ src/models/Role.ts
@@ -1,9 +1,5 @@
 import { model, Schema } from 'mongoose';
-/*
-export interface IRole {
-  value: string;
-}
-*/
+
 const schema = new Schema({
   value: {
     type: String, required: true, unique: true, default: 'teacher', lowercase: true, trim: true,
