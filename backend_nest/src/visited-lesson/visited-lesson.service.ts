1f9e06f07c474327f22adfa197f812279a5eabdd
karlsonru <ivpolovnikov@hotmail.com>
2023-04-16 19:34:11 +0300

    fix: исправления в логику абонементов

--- backend_nest/src/visited-lesson/visited-lesson.service.ts
+++ backend_nest/src/visited-lesson/visited-lesson.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { ClientSession, Model, PopulateOptions } from 'mongoose';
+import { ClientSession, Model, PopulateOptions, UpdateWriteOpResult } from 'mongoose';
 import { VisitedLessonEntity } from './entities/visited-lesson.entity';
 import { CreateVisitedLessonDto } from './dto/create-visited-lesson.dto';
 import { UpdateVisitedLessonDto } from './dto/update-visited-lesson.dto';
@@ -18,6 +18,10 @@ interface INewVisit extends Omit<Visit, 'student'> {
 interface IPrevAndNextVisits {
   prevVisit?: Visit;
   newVisit: INewVisit;
+}
+
+interface IUpdateByIdsList {
+  (ids: Array<string | undefined>, update: Record<string, number>): Promise<UpdateWriteOpResult>;
 }
 
 @Injectable()
@@ -44,17 +48,18 @@ export class VisitedLessonService {
   async create(
     createVisitedLessonDto: CreateVisitedLessonDto,
   ): Promise<VisitedLessonEntity | null> {
-    logger.debug(
-      `Обрабатываем запрос на создание нового посещённого занятия по уроку с ID: ${
-        createVisitedLessonDto.lesson
-      } за дату ${new Date(createVisitedLessonDto.date).toDateString()}`,
-    );
+    logger.debug(`
+      Обрабатываем запрос на создание нового посещённого занятия по уроку с ID:
+      ${createVisitedLessonDto.lesson} за дату 
+      ${new Date(createVisitedLessonDto.date).toDateString()}
+    `);
 
     const candidate = await this.visitedLessonModel.findOne({
       $and: [{ date: createVisitedLessonDto.date }, { lesson: createVisitedLessonDto.lesson }],
     });
 
     if (candidate) {
+      logger.debug(`Занятие уже существует`);
       return null;
     }
 
@@ -64,6 +69,8 @@ export class VisitedLessonService {
         (visit) =>
           visit.visitStatus === VisitStatus.VISITED || visit.visitStatus === VisitStatus.POSTPONED,
       );
+
+      logger.debug(`Найдено ${visitedStudents.length} посетивших или перенёсших занятие`);
 
       // найдём все абонементы студентов, посетивших занятие или у кого занятие перенесено (к отработке)
       const subscriptions = await this.subscriptionService.findAll(
@@ -81,6 +88,8 @@ export class VisitedLessonService {
           },
         },
       );
+
+      logger.debug(`Найдено ${subscriptions.length} абонементов для этих студентов`);
 
       // у всех студентов, которые посетили занятие - добавим _id абонемента с которого будет списание в визит
       const updateSubscriptions: {
@@ -103,13 +112,15 @@ export class VisitedLessonService {
       });
 
       // обновим сразу все отобранные абонементы - те что посетили уменьшим на 1
-      await this.subscriptionService.updateMany(
+      const updatedVisited = await this.subscriptionService.updateMany(
         { _id: { $in: updateSubscriptions[VisitStatus.VISITED] } },
         { $inc: { visitsLeft: -1 } },
       );
 
+      logger.debug(`Списали занятие у ${updatedVisited.modifiedCount} абонементов`);
+
       // те что посетили отложили - перенесём визит из оставшихся в отложенные
-      await this.subscriptionService.updateMany(
+      const updatedPostponed = await this.subscriptionService.updateMany(
         { _id: { $in: updateSubscriptions[VisitStatus.POSTPONED] } },
         {
           $inc: {
@@ -118,6 +129,8 @@ export class VisitedLessonService {
           },
         },
       );
+
+      logger.debug(`Перенесли занятие у ${updatedPostponed.modifiedCount} абонементов`);
 
       // сохраним] само занятие и вернём его
       return await this.visitedLessonModel.create(createVisitedLessonDto);
@@ -145,17 +158,22 @@ export class VisitedLessonService {
     id: string,
     updateVisitedLessonDto: UpdateVisitedLessonDto,
   ): Promise<VisitedLessonEntity | null> {
-    // найдём занятие, которое нужно обновить
-    const lesson = await this.findOne(id);
+    logger.debug(`Обрабатываем запрос на обновление посещённого занятия по уроку с ID: ${id}}`);
 
-    if (lesson === null) return null;
+    // найдём занятие, которое нужно обновить
+    const visitedLesson = await this.findOne(id);
+
+    if (visitedLesson === null) {
+      logger.debug(`Занятие не найдено`);
+      return null;
+    }
 
     const studentsWithChangedStatuses: Array<IPrevAndNextVisits> = [];
 
     // ищем студентов у которых статус отличается от сохранённого в БД или которых нет в БД
     updateVisitedLessonDto.students?.forEach((newVisit) => {
       // ищем предыдущий визит этого студента
-      const prevVisit = lesson.students.find(
+      const prevVisit = visitedLesson.students.find(
         (visit) => visit.student._id.toString() === newVisit.student,
       );
 
@@ -169,53 +187,66 @@ export class VisitedLessonService {
       });
     });
 
+    logger.debug(`У ${studentsWithChangedStatuses.length} студентов отличается статус`);
+
     // отфильтруем студентов у которых не искали абонемент для списывания в prevVisit
     const studentsWithoutSubscriptions = studentsWithChangedStatuses.filter(
       (visits) => !visits.prevVisit?.subscription,
     );
 
-    // найдём все абонементы студентов, у которых нет prevVisit / нет найденного абонемента с которого списать
-    const subscriptions = await this.subscriptionService.findAll(
-      {
-        $and: [
-          { lesson: id },
-          {
-            student: {
-              $in: studentsWithoutSubscriptions.map((visits) => visits.newVisit.student),
-            },
-          },
-          { visitsLeft: { $gte: 1 } },
-          { dateTo: { $gte: lesson.date } },
-        ],
-      },
-      {
-        sort: {
-          visitsLeft: 1,
-        },
-      },
-    );
+    logger.debug(`
+      У ${studentsWithoutSubscriptions.length} студентов нет информации по абонементу в предыдущем посещении
+    `);
 
-    // обновим у таких студентов subscription в prevVisit для обновления абонемента
-    studentsWithoutSubscriptions.forEach((visits) => {
-      const subscription = subscriptions.find(
-        (subscription) => subscription.student._id.toString() === visits.newVisit.student,
+    if (studentsWithoutSubscriptions.length) {
+      // найдём все абонементы студентов, у которых нет prevVisit / нет найденного абонемента с которого списать
+      console.log(`lessonId: ${visitedLesson.lesson._id.toString()}`);
+      console.log(`lessonId: ${updateVisitedLessonDto}`);
+      console.log(studentsWithoutSubscriptions.map((visits) => visits.newVisit.student));
+      const subscriptions = await this.subscriptionService.findAll(
+        {
+          $and: [
+            { lesson: visitedLesson.lesson._id.toString() },
+            {
+              student: {
+                $in: studentsWithoutSubscriptions.map((visits) => visits.newVisit.student),
+              },
+            },
+            { visitsLeft: { $gte: 1 } },
+            { dateTo: { $gte: visitedLesson.date } },
+          ],
+        },
+        {
+          sort: {
+            visitsLeft: 1,
+          },
+        },
       );
 
-      if (!subscription) return;
+      logger.debug(`Найдено ${subscriptions.length} абонементов`);
 
-      // если статус по визиту уже есть - то добавим только ID абонемента
-      if (visits.prevVisit) {
-        visits.prevVisit.subscription = subscription._id.toString();
-        return;
-      }
+      // обновим у таких студентов subscription в prevVisit для обновления абонемента
+      studentsWithoutSubscriptions.forEach((visits) => {
+        const subscription = subscriptions.find(
+          (subscription) => subscription.student._id.toString() === visits.newVisit.student,
+        );
 
-      // иначе полностью добавим prevVisit
-      visits.prevVisit = {
-        student: subscription.student,
-        visitStatus: VisitStatus.UNKNOWN,
-        subscription: subscription._id.toString(),
-      };
-    });
+        if (!subscription) return;
+
+        // если статус по визиту уже есть - то добавим только ID абонемента
+        if (visits.prevVisit) {
+          visits.prevVisit.subscription = subscription._id.toString();
+          return;
+        }
+
+        // иначе полностью добавим prevVisit
+        visits.prevVisit = {
+          student: subscription.student,
+          visitStatus: VisitStatus.UNKNOWN,
+          subscription: subscription._id.toString(),
+        };
+      });
+    }
 
     /*
     У нас могут быть следующие ситуации:
@@ -276,36 +307,52 @@ export class VisitedLessonService {
 
     // добавлям транзакцию для обновления различных статусов абонементов
     const transaction = async (session: ClientSession) => {
+      const updateByIdsList: IUpdateByIdsList = async (ids, update) => {
+        return await this.subscriptionService.updateMany({ _id: { $in: ids } }, { $inc: update });
+      };
+
       // вернём по 1 визиту в абонементы
       if (returnVisit.length) {
-        await this.subscriptionService.updateMany(
-          { _id: { $in: returnVisit.map((visit) => visit.prevVisit?.subscription) } },
-          { $inc: { visitsLeft: 1 } },
+        const returnedVisits = await updateByIdsList(
+          returnVisit.map((visit) => visit.prevVisit?.subscription),
+          { visitsLeft: 1 },
         );
+        logger.debug(`
+          В ${returnedVisits.modifiedCount} абонементах возвращен визит для ${returnVisit.length} студентов
+        `);
       }
 
       // вернём по 1 отложенному визиту в абонементы
       if (returnPostponedVisit.length) {
-        await this.subscriptionService.updateMany(
-          { _id: { $in: returnPostponedVisit.map((visit) => visit.prevVisit?.subscription) } },
-          { $inc: { vistsPostponed: 1 } },
+        const returnedPostponed = await updateByIdsList(
+          returnPostponedVisit.map((visit) => visit.prevVisit?.subscription),
+          { vistsPostponed: 1 },
         );
+        logger.debug(`
+          В ${returnedPostponed.modifiedCount} абонементах возвращено отложенное занятие для ${returnPostponedVisit.length} студентов
+        `);
       }
 
       // добавим по 1  визиту в абонементы
       if (addVisit.length) {
-        await this.subscriptionService.updateMany(
-          { _id: { $in: addVisit.map((visit) => visit.prevVisit?.subscription) } },
-          { $inc: { visitsLeft: -1 } },
+        const addedVisits = await updateByIdsList(
+          addVisit.map((visit) => visit.prevVisit?.subscription),
+          { visitsLeft: -1 },
         );
+        logger.debug(`
+          В ${addedVisits.modifiedCount} абонементах списано занятие для ${addVisit.length} студентов
+        `);
       }
 
       // добавим по 1 отложенному визиту в абонементы
       if (addPostponedVisit.length) {
-        await this.subscriptionService.updateMany(
-          { _id: { $in: addPostponedVisit.map((visit) => visit.prevVisit?.subscription) } },
-          { $inc: { visitsLeft: -1 } },
+        const addedPostponedVisits = await updateByIdsList(
+          addPostponedVisit.map((visit) => visit.prevVisit?.subscription),
+          { vistsPostponed: -1 },
         );
+        logger.debug(`
+          В ${addedPostponedVisits.modifiedCount} абонементах списано отложенное занятие для ${addPostponedVisit.length} студентов
+        `);
       }
 
       // обновим занятие и вернём результат
